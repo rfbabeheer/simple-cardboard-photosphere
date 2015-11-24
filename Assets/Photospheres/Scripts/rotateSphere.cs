fe475836323761c44b2d8b5b70342afff946f1ae
Riva Farabi <rivafarabi.babeheer@gmail.com>
2015-11-25 01:20:19 +0700

    Add files

---
+++ Assets/Photospheres/Scripts/rotateSphere.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateSphere : MonoBehaviour {
+
+	[SerializeField] private float speedY;
+	private bool onRotate;
+
+
+	// Use this for initialization
+	void Start () {
+		speedY = 5f;
+		onRotate = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Cardboard.SDK.Triggered) {
+			if(onRotate){
+				onRotate = false;
+			}
+			else{
+				onRotate = true;
+			}
+		}
+		if (onRotate) {
+			transform.Rotate (new Vector3 (0, speedY, 0) * Time.deltaTime);
+		}
+	}
+}
